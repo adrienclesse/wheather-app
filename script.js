2636a4dd7ed27f17d29f5253066c92cd83ab59eb
adrienclesse <adrienclesse@hotmail.com>
2020-12-03 16:53:39 +0100

    fix bugg icons 1

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ document.getElementById("submit").addEventListener("click",function(){
             document.getElementById('tempday2').innerHTML = data.data[2].temp + '&deg;';
             document.getElementById('tempday3').innerHTML = data.data[3].temp + '&deg;';
             document.getElementById('tempday4').innerHTML = data.data[4].temp + '&deg;';
-            document.getElementById('tempday5').innerHTML = data.data[0].temp + '&deg;';
+            document.getElementById('tempday5').innerHTML = data.data[5].temp + '&deg;';
 
 
 
@@ -60,18 +60,24 @@ document.getElementById("submit").addEventListener("click",function(){
                     else if(weather==="Few clouds"){logo.src = "img/cloud-sun.png";}
                     else if(weather==="Light shower rain"){logo.src = "img/rain.png";}
                     else if(weather==="Moderate rain"){logo.src = "img/rain.png";}
+                    
+
                    
             }
             getLogo(document.getElementById("logo"),document.getElementById("description").innerHTML);
             getLogo(document.getElementById("logoday1"),document.getElementById("descriptionday1").innerHTML);
             getLogo(document.getElementById("logoday2"),document.getElementById("descriptionday2").innerHTML);
             getLogo(document.getElementById("logoday3"),document.getElementById("descriptionday3").innerHTML);
-            getLogo(document.getElementById("logoday4"),document.getElementById("description").innerHTML);
-            getLogo(document.getElementById("logoday5"),document.getElementById("description").innerHTML);
+            getLogo(document.getElementById("logoday4"),document.getElementById("descriptionday4").innerHTML);
+            getLogo(document.getElementById("logoday5"),document.getElementById("descriptionday5").innerHTML);
               
 
 
         }
+
+
+
+
 (function(){
         let name1= "paris";
         weatherBalloon(name1);
